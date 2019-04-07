4613c131f14fcbfe52416ed5863e820a40852adc
宋连盟 <songlianmeng@dding.cn>
2019-04-07 12:54:19 +0800

    fix：定时

--- schedule/order_downline.js
+++ schedule/order_downline.js
@@ -4,9 +4,9 @@ const redis = require('../lib/redis');
 const moment = require('moment');
 
 // 每天0点执行
-const j = schedule.scheduleJob('1 0 0 * * *', async function() {
-  console.log('schedule start')
+schedule.scheduleJob('1 0 0 * * *', async function() {
   const yesterday = moment().subtract(1, 'day').format('YYYY-MM-DD');
+  console.log('statistics date', yesterday)
   // 渠道活跃用户数统计
   const channelArray = await db.collection('channel').find().toArray();
   for (const channel of channelArray) {
@@ -19,7 +19,6 @@ const j = schedule.scheduleJob('1 0 0 * * *', async function() {
         createAt: new Date(), 
         updateAt: new Date()
       });
-      console.log('insert channelStat one')
     }
   }
 
@@ -47,8 +46,6 @@ const j = schedule.scheduleJob('1 0 0 * * *', async function() {
       createAt: new Date(), 
       updateAt: new Date()
     });
-    console.log('insert orderStat one')
-
   }
   // 自动下线
   await db.collection('order').updateOne({ endDate: { $lte: new Date()}}, { $set: { online: 0 } });
